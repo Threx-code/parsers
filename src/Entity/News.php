ebc04180935f788474d2ad5bc9a51c227777c38d
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2022-11-15 08:53:59 +0100

    #updating the entity and first test script

---
+++ src/Entity/News.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=NewsRepository::class)
+ */
+class News
+{
+    /**
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $title = null;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private ?string $description = null;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $image = null;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private ?\DateTimeInterface $date_added = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDateAdded(): ?\DateTimeInterface
+    {
+        return $this->date_added;
+    }
+
+    public function setDateAdded(\DateTimeInterface $date_added): self
+    {
+        $this->date_added = $date_added;
+
+        return $this;
+    }
+}
---
+++ tests/unit/ApiParsersTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\unit;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ApiParsersTest extends KernelTestCase
+{
+
+    public function testItWorks()
+    {
+        return $this->assertTrue(true);
+    }
+
+}
